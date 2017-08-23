36ac78284bc2d77a39f27ac128f4ec9473528916
Kevin Fallas <kev.fallas@gmail.com>
2017-08-22 19:59:43 -0600

    Implementing holydays logic

--- script.js
+++ script.js
@@ -31,9 +31,9 @@
     function getInitialDayToDraw(date) {
         var day = -1;
         var dateStr = date.toString();
-        console.log(dateStr);
+        //console.log(dateStr);
         day = dateStr.split(" ")[2];
-        console.log(day);
+        //console.log(day);
         return day;
     }
 
@@ -49,17 +49,17 @@
         var inputDate = new Date(date);
         // add a day
         inputDate.setDate(inputDate.getDate() + 1);
-        
-            //Do Calendars
-            //getWeekdaysHeader();
-            displayCalendar(inputDate, numberOfDays, countryCode);
+
+        //Do Calendars
+        //getWeekdaysHeader();
+        displayCalendar(inputDate, numberOfDays, countryCode);
     });
 
 
 
     function displayCalendar(inputDate, numberOfDays, countryCode) {
 
-        
+
 
 
         /*console.log("iD: " + inputDate + "\n initialDayMonth: "+initialDayMonth + "\n numbdays: "+numberOfDays+"\n month: "+month+" "+year+"\n idtd: "+initialDayToDraw);*/
@@ -69,17 +69,50 @@
 
     }
 
+    var data = [];
+    
+    function getMonthHolidays(y, m) {
+        
+        var xhr = new XMLHttpRequest();        
+        var APIurl = "https://holidayapi.com/v1/holidays?country=US&year="+y+"&month="+m+"&key=d7067658-2852-4cb4-b0df-56daac7cbf80";
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
 
+                data = JSON.parse(xhr.response);
+                
+            }
+        }
+        xhr.open('GET', APIurl, false);
+        xhr.send(null);
+    }
 
+    
+    function isHoliday(d){
+        
+        for(var i = 0; i< data.holidays.length; i++){
+            if(data.holidays[i].date.split("-")[2] == d)
+                return data.holidays[i].name;
+        }
+        
+        return false;
+    }
+    
+    
     function drawCalendar(inputDate, daystoDraw) {
         var style = "weekdays";
-        
+
         var month = inputDate.getMonth();
         var year = inputDate.getFullYear();
         
+        getMonthHolidays(year, month);
+        
+
         var initialDayMonth = getFirstDayOfTheMonth(inputDate);
         var initialDayToDraw = getInitialDayToDraw(inputDate);
         
+        var holidayName = -1;
+        var title ="";
+
         var cellValue;
         var t = '<div><table cols="7" cellpadding="0" cellspacing="0" class="month-container"><tr align="center" class="daysofweek">';
 
@@ -87,51 +120,69 @@
 
         t += '</tr><tr align="center">';
 
-        
+
         t += '<td colspan="7" align="center" class="month-label">' + monthsArray[month] + ' - ' + year + '</td></tr><tr align="center">';
         t += '</tr><tr align="center">';
 
 
-        for (i = 1; i <= 42; i++) {            
-            
+        for (i = 1; i <= 42; i++) {
+
             if (cellValue === '&nbsp;' && (i > initialDayMonth) && limitDraw.indexOf(i) > -1) {
-                 break;
-             }
+                break;
+            }
 
             if ((i >= initialDayMonth) && (initialDayToDraw <= getMonthDays(month, year)) && (daystoDraw > 0)) {
                 cellValue = initialDayToDraw;
+                            
+                
                 daystoDraw--;
                 initialDayToDraw++;
-                style = ((i) % 7 == 0) || ((i-1) % 7 == 0) ? "weekend" : "weekdays";
+                
+                
+                holidayName = isHoliday(initialDayToDraw);
+                
+                
+                if(holidayName){
+                   style = "holiday";
+                    title = 'title="'+holidayName+'"';
+               }else{
+                    style = ((i) % 7 == 0) || ((i - 1) % 7 == 0) ? "weekend" : "weekdays";
+                   title = "";
+               }
+                
+                
+                
+                
+                
             } else {
                 cellValue = '&nbsp;';
                 style = "invalid-day";
             }
 
             //= ((i > dayToStart) && (i <= monthDimension) ) ? i : '&nbsp;';
-            
-            
 
-            t += '<td class="'+style+'">' + cellValue + '</td>';
+
+
+            t += '<td class="' + style + '"'+title+'">' + cellValue + '</td>';
 
             if ((i) % 7 == 0)
                 t += '</tr><tr align="center">';
 
         }
         t += '</tr></table></div>';
-        
+
         document.querySelector(".display-calendar").insertAdjacentHTML("beforeend", t);
-        
-        if(daystoDraw > 0){
+
+        if (daystoDraw > 0) {
             var nextMonth = month === 11 ? 1 : month + 1;
             var year = nextMonth === 1 ? year + 1 : year;
-            
-            var d = new Date(year,nextMonth,01);
-            
+
+            var d = new Date(year, nextMonth, 01);
+
             drawCalendar(d, daystoDraw);
         }
     }
-     
+
 
 })();
 
