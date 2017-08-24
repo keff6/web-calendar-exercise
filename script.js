cab3968c103c8d2605eaf8a3e4f7e3c728a3f492
Kevin Fallas <kev.fallas@gmail.com>
2017-08-23 19:24:55 -0600

    Form validations and styling

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // main function
-(function () {
+(function (win, doc) {
 
-    const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const limitDraw = [8, 15, 22, 28, 36, 43];
+    const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        limitDraw = [8, 15, 22, 28, 36, 43],
+        DOM_countryCode = doc.getElementById("countryCode"),
+        DOM_dateSelected = doc.getElementById("date"),
+        DOM_numberOfDays = doc.getElementById("numberOfDays");
+
     var monthDimension = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     var data = [];
-
-    var btnGenerate = document.getElementById("btn-generate");
 
 
     function getMonthDays(month, year) {
@@ -41,10 +43,10 @@
         var APIurl = "https://holidayapi.com/v1/holidays?country=" + c + "&year=" + y + "&month=" + m + "&key=d7067658-2852-4cb4-b0df-56daac7cbf80";
 
         var xhr = new XMLHttpRequest();
-
+        xhr.open('GET', APIurl, false);
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
-               if (xhr.status == 200) {
+                if (xhr.status == 200) {
                     try {
                         data = JSON.parse(xhr.response);
                     } catch (e) {
@@ -66,7 +68,7 @@
                 */
             }
         }
-        xhr.open('GET', APIurl, false);
+
         xhr.send(null);
     }
 
@@ -83,104 +85,151 @@
 
 
     function drawCalendar(inputDate, daystoDraw, countryCode) {
-        var style = "weekdays";
 
-        var month = inputDate.getMonth();
-        var year = inputDate.getFullYear();
+        var month = inputDate.getMonth(),
+            year = inputDate.getFullYear(),
+            initialDayMonth = getFirstDayOfTheMonth(inputDate),
+            initialDayToDraw = getInitialDayToDraw(inputDate),
+            holidayName = -1,
+            title = "",
+            style = "weekdays",
+            rowClosing = '</tr><tr align="center">',
+            cellValue, htmlText;
 
         getMonthHolidays(year, month);
 
 
-        var initialDayMonth = getFirstDayOfTheMonth(inputDate);
-        var initialDayToDraw = getInitialDayToDraw(inputDate);
+        htmlText = '<div><table cols="7" cellpadding="0" cellspacing="0" class="month-container"><tr align="center" class="daysofweek">';
 
-        var holidayName = -1;
-        var title = "";
+        // Calendar days labe header
+        for (var s = 0; s < 7; s++) htmlText += '<td class="day-label">' + "SMTWTFS".substr(s, 1) + '</td>';
 
-        var cellValue;
-        var t = '<div><table cols="7" cellpadding="0" cellspacing="0" class="month-container"><tr align="center" class="daysofweek">';
+        htmlText += rowClosing;
 
-        for (s = 0; s < 7; s++) t += '<td class="day-label">' + "SMTWTFS".substr(s, 1) + '</td>';
+        // Month name header
+        htmlText += '<td colspan="7" align="center" class="month-label">' + monthsArray[month] + ' - ' + year + '</td></tr><tr align="center">';
 
-        t += '</tr><tr align="center">';
-
-
-        t += '<td colspan="7" align="center" class="month-label">' + monthsArray[month] + ' - ' + year + '</td></tr><tr align="center">';
-        t += '</tr><tr align="center">';
+        htmlText += rowClosing;
 
 
+        // Days draw
         for (i = 1; i <= 42; i++) {
 
+            //Closing rules
             if (cellValue === '&nbsp;' && (i > initialDayMonth) && limitDraw.indexOf(i) > -1) {
                 break;
             }
 
-            if ((i >= initialDayMonth) && (initialDayToDraw <= getMonthDays(month, year)) && (daystoDraw > 0)) {
-                cellValue = initialDayToDraw;
 
+            if ((i >= initialDayMonth) &&
+                (initialDayToDraw <= getMonthDays(month, year)) &&
+                (daystoDraw > 0)) {
+                try {
+                    // Valid day
+                    cellValue = initialDayToDraw;
+                    daystoDraw--;
+                    initialDayToDraw++;
 
-                daystoDraw--;
-                initialDayToDraw++;
+                    // Holiday searching
+                    holidayName = isHoliday(initialDayToDraw);
 
-
-                holidayName = isHoliday(initialDayToDraw);
-
-
-                if (holidayName) {
-                    style = "holiday";
-                    title = 'title="' + holidayName + '"';
-                } else {
-                    style = ((i) % 7 == 0) || ((i - 1) % 7 == 0) ? "weekend" : "weekdays";
-                    title = "";
+                    if (holidayName) {
+                        style = "holiday";
+                        title = 'title="' + holidayName + '"';
+                    } else {
+                        style = ((i) % 7 == 0) || ((i - 1) % 7 == 0) ? "weekend" : "weekdays";
+                        title = "";
+                    }
+                } catch (e) {
+                    alert(e.message);
                 }
-
-
-
-
-
             } else {
+                //invalid day
                 cellValue = '&nbsp;';
                 style = "invalid-day";
             }
 
-            //= ((i > dayToStart) && (i <= monthDimension) ) ? i : '&nbsp;';
-
-
-
-            t += '<td class="' + style + '"' + title + '">' + cellValue + '</td>';
+            htmlText += '<td class="' + style + '"' + title + '">' + cellValue + '</td>';
 
             if ((i) % 7 == 0)
-                t += '</tr><tr align="center">';
+                htmlText += rowClosing;
 
         }
-        t += '</tr></table></div>';
+        htmlText += '</tr></table></div>';
 
-        document.querySelector(".display-calendar").insertAdjacentHTML("beforeend", t);
+        //insert the calendar into html
+        doc.querySelector(".display-calendar").insertAdjacentHTML("beforeend", htmlText);
 
+        // If there are days to draw left, then call drawCalendar recursively
         if (daystoDraw > 0) {
-            var nextMonth = month === 11 ? 1 : month + 1;
-            var year = nextMonth === 1 ? year + 1 : year;
+            var nextMonth = month === 11 ? 0 : month + 1,
+                year = nextMonth === 1 ? year + 1 : year,
+                d = new Date(year, nextMonth, 01);
 
-            var d = new Date(year, nextMonth, 01);
-
-            drawCalendar(d, daystoDraw);
+            drawCalendar(d, daystoDraw, countryCode);
         }
     }
 
-    btnGenerate.addEventListener("click", function () {
+    function clearPage() {
 
-        var date = document.getElementById("date").value;
+        /*numberOfDays.value = "";
+        countryCode.value = "";
+        dateSelected.value = "";*/
+        
+        var elem = doc.querySelector(".display-calendar");
+        elem.innerHTML = "";
+        return false;
+    }
 
-        var numberOfDays = document.getElementById("numberOfDays").value;
-        var countryCode = document.getElementById("countryCode").value;
-        var inputDate = new Date(date);
-        // add a day
-        inputDate.setDate(inputDate.getDate() + 1);
+    function validateInput() {
 
-        //Do Calendars
-        //getWeekdaysHeader();
-        drawCalendar(inputDate, numberOfDays, countryCode);
-    });
+        var isValid = true;
+        var message = "";
+        
+        if(!DOM_numberOfDays.checkValidity()){
+            message += "<p>You have to enter a valid number of days.<p>"
+        }
+        if(!DOM_countryCode.checkValidity()){
+            message += "<p>You have to enter a valid Country code.<p>"
+        }
+        if(!DOM_dateSelected.checkValidity()){
+            message += "<p>You have to enter a valid date.<p>"
+        }
+        
+        if(message.length > 0){
+            isValid = false;
+            doc.querySelector(".warning-container").innerHTML = message;
+            
+            doc.querySelector(".warning-container").style.display = "block";
+        }else{
+            doc.querySelector(".warning-container").style.display = "none";
+        }
+        
+        return isValid;
+    }
+
+    win.onload = function () {
+
+        var btnGenerate = doc.getElementById("btn-generate");
+
+        btnGenerate.addEventListener("click", function () {            
+
+            clearPage();
+            
+            if (validateInput()) {               
+                
+                var date = DOM_dateSelected.value,
+                    numberOfDays = DOM_numberOfDays.value,
+                    countryCode = DOM_countryCode.value,
+                    inputDate = new Date(date);
+
+                inputDate.setDate(inputDate.getDate() + 1);
+
+                drawCalendar(inputDate, numberOfDays, countryCode);
+            }
+
+        });
+    }
 
 
-})();
+})(window, document);
